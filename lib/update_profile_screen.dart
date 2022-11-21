9ef415d1824d474619656442c80d9b7a82ee52c1
Rishbah-76 <rbrishabh76@gmail.com>
2022-11-21 17:32:11 +0530

    update_profile_screen completed

--- lib/update_profile_screen.dart
+++ lib/update_profile_screen.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UpdateProfileScreen extends StatelessWidget {
-  const UpdateProfileScreen({super.key});
+  UpdateProfileScreen({super.key});
+
+  final TextEditingController name = TextEditingController();
+  final TextEditingController email = TextEditingController();
+
+  //to Store the data in local storage
+  Future<void> saveDataToLocal(String key, String value) async {
+    //Initializing thr shared_preferences instance
+    SharedPreferences _pref = await SharedPreferences.getInstance();
+    await _pref.setString(key, value);
+  }
+
+  //To retrive the data from TextField annd store in local storage
+  void saveUserDetails() async {
+    await saveDataToLocal('name', name.text);
+    await saveDataToLocal('email', email.text);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,15 +28,15 @@ class UpdateProfileScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          customTextfield("Name"),
-          customTextfield("Email"),
+          customTextfield("Name", name),
+          customTextfield("Email", email),
           ElevatedButton(onPressed: () {}, child: const Text("Update Details"))
         ],
       ),
     );
   }
 
-  Widget customTextfield(String title) {
+  Widget customTextfield(String title, TextEditingController controller) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: TextField(
